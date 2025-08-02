3a519c4d3dc9c453f1af9bb0f1528e16d60e65b0
YearsAlso <1334713380@qq.com>
2025-08-02 17:36:51 +0800

    fix(classic-interview-010): 修复跳台阶算法的逻辑错误
    - 修正了初始位置能到达最远位置时的返回值
    - 优化了算法逻辑，提高了执行效率
    - 调整了代码格式，提高了可读性

--- classic-interview/classic-interview-010/src/main/java/com/yearsalso/Solution.java
+++ classic-interview/classic-interview-010/src/main/java/com/yearsalso/Solution.java
@@ -2,23 +2,23 @@ package com.yearsalso;
 
 /**
  * 给定一个长度为 n 的 0 索引整数数组 nums。初始位置为 nums[0]。
- *
+ * <p>
  * 每个元素 nums[i] 表示从索引 i 向后跳转的最大长度。换句话说，如果你在 nums[i] 处，你可以跳转到任意 nums[i + j] 处:
- *
+ * <p>
  * 0 <= j <= nums[i]
  * i + j < n
  * 返回到达 nums[n - 1] 的最小跳跃次数。生成的测试用例可以到达 nums[n - 1]。
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: nums = [2,3,1,1,4]
  * 输出: 2
  * 解释: 跳到最后一个位置的最小跳跃数是 2。
- *      从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+ * 从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
  * 示例 2:
- *
+ * <p>
  * 输入: nums = [2,3,0,1,4]
  * 输出: 2
  */
@@ -26,24 +26,28 @@ public class Solution {
 
     public int jump(int[] nums) {
         if (nums.length <= 1) {
-            return 0;
+            return 1;
         }
 
         int maxReach = nums[0]; // 记录当前能到达的最远位置
 
-        int minSteps = 1; // 记录当前跳跃次数
+        int minSteps = 0; // 记录当前跳跃次数
+
+        if (maxReach >= nums.length - 1) {
+            return 1; // 如果初始位置就能到达或超过最后一个位置，直接返回1
+        }
 
         for (int i = 1; i < nums.length; i++) {
             // 更新能到达的最远位置
             maxReach = Math.max(maxReach, i + nums[i]);
-            if(maxReach == i + nums[i]) {
+            if (maxReach == i + nums[i]) {
                 // 如果当前位置的最大跳跃长度等于当前下标，说明需要跳跃
                 minSteps++;
             }
 
             // 如果已经能到达或超过最后一个下标，直接返回true
             if (maxReach >= nums.length - 1) {
-                minSteps ++; // 最后一次跳跃
+                minSteps++; // 最后一次跳跃
                 break;
             }
         }

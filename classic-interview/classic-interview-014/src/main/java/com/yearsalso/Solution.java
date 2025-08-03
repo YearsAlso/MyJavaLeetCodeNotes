1497ce6e32dc266792a1cd3722a34fe1200f4921
YearsAlso <1334713380@qq.com>
2025-08-03 16:37:14 +0800

    refactor(classic-interview):优化加油站问题解决方案
    - 添加新的解决方法 canCompleteCircuit，提高了算法效率
    - 优化了代码格式和注释，提高了代码可读性
    - 更新了 .gitignore 文件，添加了更多需要忽略的文件类型

--- classic-interview/classic-interview-014/src/main/java/com/yearsalso/Solution.java
+++ classic-interview/classic-interview-014/src/main/java/com/yearsalso/Solution.java
@@ -2,15 +2,15 @@ package com.yearsalso;
 
 /**
  * 在一条环路上有 n 个加油站，其中第 i 个加油站有汽油 gas[i] 升。
- *
+ * <p>
  * 你有一辆油箱容量无限的的汽车，从第 i 个加油站开往第 i+1 个加油站需要消耗汽油 cost[i] 升。你从其中的一个加油站出发，开始时油箱为空。
- *
+ * <p>
  * 给定两个整数数组 gas 和 cost ，如果你可以按顺序绕环路行驶一周，则返回出发时加油站的编号，否则返回 -1 。如果存在解，则 保证 它是 唯一 的。
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
  * 输出: 3
  * 解释:
@@ -22,7 +22,7 @@ package com.yearsalso;
  * 开往 3 号加油站，你需要消耗 5 升汽油，正好足够你返回到 3 号加油站。
  * 因此，3 可为起始索引。
  * 示例 2:
- *
+ * <p>
  * 输入: gas = [2,3,4], cost = [3,4,3]
  * 输出: -1
  * 解释:
@@ -34,24 +34,24 @@ package com.yearsalso;
  * 因此，无论怎样，你都不可能绕环路行驶一周。
  */
 public class Solution {
-    public int canCompleteCircuitWithStart(int start, int[] subGas){
+    public int canCompleteCircuitWithStart(int start, int[] subGas) {
         int index = start;
         int totalGas = 0;
-        while(true){
+        while (true) {
             totalGas += subGas[index];
-            if(totalGas < 0){
+            if (totalGas < 0) {
                 return -1; // 如果在某个点油量不足，返回-1
             }
 
             index = (index + 1) % subGas.length; // 环形移动
-            if(index == start){ // 如果回到起点，说明可以完成一圈
+            if (index == start) { // 如果回到起点，说明可以完成一圈
                 return start;
             }
         }
     }
 
 
-    public int canCompleteCircuit(int[] gas, int[] cost) {
+    public int canCompleteCircuit2(int[] gas, int[] cost) {
         int[] subGas = new int[gas.length];
         if (gas.length == 0) {
             return -1; // 如果没有加油站，返回-1
@@ -66,14 +66,37 @@ public class Solution {
         }
 
         for (int i = 0; i < subGas.length; i++) {
-            if(subGas[i] >= 0) { // 如果当前点的油量足够
+            if (subGas[i] >= 0) { // 如果当前点的油量足够
                 int result = canCompleteCircuitWithStart(i, subGas);
-                if(result != -1) {
+                if (result != -1) {
                     return result; // 返回可以完成一圈的起始点
                 }
             }
         }
 
         return -1; // 如果没有找到可以完成一圈的起始点，返回-1
+    }
+
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0;
+        int totalCost = 0;
+        int currentGas = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+            currentGas += gas[i] - cost[i];
+
+            // 如果当前油量为负，说明无法从start到达i+1
+            // 根据优化思路，start到i之间的任何点都无法完成环路
+            if (currentGas < 0) {
+                start = i + 1;  // 从下一个站点重新开始
+                currentGas = 0; // 重置当前油量
+            }
+        }
+
+        // 如果总油量小于总消耗，无解
+        return totalGas >= totalCost ? start : -1;
     }
 }
